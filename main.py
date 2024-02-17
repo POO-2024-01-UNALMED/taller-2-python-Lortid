9e3721fc72a524f0713b1ee18775a61810808cf2
Jaime Luis Osorio Gómez <jaosoriogo@unal.edu.co>
2024-02-16 19:48:57 -0500

    Primera versión

---
+++ main.py
@@ -0,0 +1,67 @@
+class Asiento:
+    
+    def __init__(self, color, precio, registro) :
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(self,color_auto):
+
+        list = ['rojo', 'verde', 'amarillo', 'negro', 'blanco']
+        
+        if color_auto in list:
+
+            self.color=color_auto
+
+class Motor :
+        
+    def __init__(self,numeroCilindros, tipo, registro) :
+            
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+            
+    def cambiarRegistro (self, registro_auto):
+            
+        self.registro = registro_auto
+            
+    def asignarTipo (self, tipo_auto):
+            
+        if tipo_auto == 'electrico' or tipo_auto == 'gasolina' :
+                
+            self.tipo = tipo_auto
+            
+class Auto :
+    
+    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+        
+        cantidadCreados 
+        self.modelo = modelo
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        self.precio = precio
+        
+    def cantidadAsientos(self, cantAsientos, asientos):
+        
+        for i in range(0, len(asientos)):
+            
+            if isinstance(asientos[i], Asiento):
+                
+                cantAsientos +=1
+                
+                
+        
+        
+        
+
+        
+        
+        
+        
+        
+        
+                
+
+            
